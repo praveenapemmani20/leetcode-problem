88787f35d56499533252133ee5863ee4c5a88ee9
praveenapemmani20 <pemmanipraveena@gmail.com>
2025-02-22 10:31:24 +0530

    day-3(Remove-duplicates)

---
+++ Remove-duplicates.java
@@ -0,0 +1,16 @@
+class Remove-Duplicates {
+    public int removeDuplicates(int[] nums) {
+        int k=0;
+       
+                for(int j=1;j<nums.length;j++)
+                    {
+                        if(nums[k]!=nums[j])
+                            {
+                                k++;
+                                nums[k]=nums[j];
+                            }
+                    }
+            
+            return k+1;
+    }
+}
