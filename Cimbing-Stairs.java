e0d6739d503eb5b0ed5e7d6ee65f85b3fccf30a7
praveenapemmani20 <pemmanipraveena@gmail.com>
2025-02-26 11:49:39 +0530

    day-7(ClimbingStairs)

---
+++ Cimbing-Stairs.java
@@ -0,0 +1,15 @@
+class Cimbing-Stairs {
+    public int climbStairs(int n) {
+        if(n==1)return 1;
+        if(n==2)return 2;
+        int first=1;
+        int second=2;
+        for(int i=3;i<=n;i++)
+        {
+            int tem=first+second;
+            first=second;
+            second=tem;
+        }
+        return second;
+    }
+}
