92442548a5e85ca5e9c70cd2c53c8d59133e5ffd
praveenapemmani20 <pemmanipraveena@gmail.com>
2025-03-25 17:34:53 +0530

    Count Pairs Whose Sum is Less than Target(day-13)

---
+++ Count_pairs_whose_sum_is_lessthan_target.java
@@ -0,0 +1,14 @@
+class  Count_pairs_whose_sum_is_lessthan_target{
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int c=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]<target){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+};
